e44a4e81888e5825f445ae7f80be77a26c5c6950
Diksha Berar <dikshaberar@gmail.com>
2026-03-13 22:56:08 +0530

    Fix port for Render

--- app.py
+++ app.py
@@ -63,5 +63,7 @@ def get_scores():
 
 if __name__ == '__main__':
     init_db()
-    print("Running at http://localhost:5000")
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    print("Running at http://localhost:" + str(port))
+    app.run(host='0.0.0.0', port=port, debug=False)
